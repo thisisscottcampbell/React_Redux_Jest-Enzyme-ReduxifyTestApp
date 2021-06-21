f4b283f00621d779ff714dfe35e42509d7233ca8
Scott Campbell <thisisscottcampbell@gmail.com>
2021-06-21 17:20:22 -0400

    enz

--- src/components/__test__/App.test.js
+++ src/components/__test__/App.test.js
@@ -2,12 +2,21 @@ import React from 'react';
 import { shallow, configure } from 'enzyme';
 import App from '../App';
 import CommentBox from '../CommentBox';
+import CommentList from '../CommentList';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 configure({ adapter: new Adapter() });
 
-it('shows a comment box', () => {
-	const wrapped = shallow(<App />);
+let wrapped;
 
+beforeEach(() => {
+	wrapped = shallow(<App />);
+});
+
+it('shows a comment box', () => {
 	expect(wrapped.find(CommentBox).length).toEqual(1);
+});
+
+it('shows a comment list', () => {
+	expect(wrapped.find(CommentList).length).toEqual(1);
 });
